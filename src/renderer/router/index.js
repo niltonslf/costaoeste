abc261da3f8046b3e78b3b6afa5c1e291ffbceca
Nilton Lopes <nilton-rck@hotmail.com>
2018-06-17 17:09:51 -0300

    Initial commit

---
+++ src/renderer/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: require('@/components/Home').default
+    },
+    {
+      path: '/location',
+      name:'locacao',
+      component: require('@/components/location/Location').default
+    }
+  ]
+})
