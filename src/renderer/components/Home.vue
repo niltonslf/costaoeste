72b0233f8690e170327073fd835e349f450555d2
Nilton Lopes <nilton-rck@hotmail.com>
2018-06-23 02:45:49 -0300

    Add Card component

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,258 +1,38 @@
 <template>
   <div class="d-flex justify-content-between flex-wrap align-items-center">
 
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 01</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <router-link :to="{ name: 'locacao'}" class="btn btn-sm btn-primary">Ver locação</router-link>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card vacant">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
-
-    <div class="card busy">
-      <div class="card-body">
-        <h5 class="card-title">Quarto: 00</h5>
-        <div class="card-text">
-          <ul>
-            <li><b>Hospede:</b> João da silva</li>
-            <li><b>Entrada:</b> 15/04/2018</li>
-            <li><b>Saida prevista:</b> 20/04/2018</li>
-          </ul>
-        </div>
-        <a href="/location" class="btn btn-sm btn-primary">Ver locação</a>
-      </div>
-    </div>
+    <card status="vacant"></card>
+    <card status="busy"></card>
+    <card status="reserved"></card>
+    <card status="vacant"></card>
+    <card status="busy"></card>
+    <card status="reserved"></card>
+    <card status="vacant"></card>
+    <card status="vacant"></card>
+    <card status="vacant"></card>
+    <card status="vacant"></card>
+    <card status="busy"></card>
+    <card status="reserved"></card>
 
   </div>
 </template>
 
 <script>
+import Card from './card/Card.vue';
 
+export default{
+  components: {
+    Card,
+  },
+
+  data(){
+    return{
+
+    }
+  }
+}
 </script>
 
 <style lang="css" scoped="true">
-.card{
-  width: calc(25% - 10px);
-  margin: 5px;
-  box-sizing: border-box;
-  border-width: 2px;
-}
-ul{
-  padding: 0;
-}
 
-.card.vacant{
-  border-color: green;
-
-}
-
-.card.busy{
-  border-color: red;
-}
-
-.btn{
-  width:100%;
-}
 </style>
---
+++ src/renderer/components/card/Card.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+  <div :class="'card ' + status">
+    <div class="card-body">
+      <h5 class="card-title">Quarto: 01</h5>
+      <div class="card-text">
+        <ul>
+          <li><b>Hospede:</b> João da silva</li>
+          <li><b>Entrada:</b> 15/04/2018</li>
+          <li><b>Saida prevista:</b> 20/04/2018</li>
+        </ul>
+      </div>
+      <router-link :to="{ name: 'location'}" class="btn btn-sm btn-primary">Ver locação</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['status'],
+  data(){
+    return {
+    }
+  }
+}
+</script>
+
+
+<style lang="css">
+.card{
+  width: calc(25% - 10px);
+  margin: 5px;
+  box-sizing: border-box;
+  border-width: 2px;
+}
+ul{
+  padding: 0;
+}
+
+.card.vacant{
+  border-color: green;
+  background-color: green;
+  color: white;
+}
+
+.card.busy{
+  border-color: red;
+  background-color: red;
+  color: white;
+}
+
+.card.reserved{
+  border-color: orange;
+  background-color: orange;
+  color: white;
+}
+
+.card .btn{
+  width:100%;
+  background-color: white;
+  border-color: #666;
+  color: #666
+}
+</style>
